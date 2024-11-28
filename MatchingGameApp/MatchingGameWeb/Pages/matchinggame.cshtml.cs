3aacd4b01fee5c77df9637119baed8aad75fcf27
yochevedg <yochevedgoldberg1@gmail.com>
2024-11-27 20:55:00 -0500

    committed matching game

---
+++ MatchingGameApp/MatchingGameWeb/Pages/matchinggame.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MatchingGameWeb.Pages
+{
+    public class matchinggameModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
